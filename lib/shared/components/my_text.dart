237a00f7cc19d9c9d6ef8227e719110b74ac7753
Mohamed-Shalabi <mshalaby10@gmail.com>
2022-09-07 18:58:52 +0200

    removed localization and added shared preferences

--- lib/shared/components/my_text.dart
+++ lib/shared/components/my_text.dart
@@ -1,14 +1,10 @@
-import 'package:e_commerce/shared/styles/app_themes.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyText extends StatelessWidget {
   const MyText(
     this.text, {
     Key? key,
     this.textStyle,
-    this.isTranslated = true,
     this.textAlign = TextAlign.start,
     this.overflow,
     this.maxLines,
@@ -16,7 +12,6 @@ class MyText extends StatelessWidget {
 
   final String text;
   final TextStyle? textStyle;
-  final bool isTranslated;
   final TextAlign textAlign;
   final TextOverflow? overflow;
   final int? maxLines;
@@ -24,7 +19,7 @@ class MyText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      isTranslated ? text.tr() : text,
+      text,
       style: textStyle,
       textAlign: textAlign,
       overflow: overflow,
